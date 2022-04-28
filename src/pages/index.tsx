04b628fa448c8afd626c2695b5cf8179aa1380f7
qazal <ghazal.nzhrzr@gmail.com>
2022-04-28 09:34:41 +0430

    feat(pages): use SWR instead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,25 @@
 import { Center, Text, Button } from "@chakra-ui/react";
 import { Layout, GasView, DarkModeSwitch } from "@etherTrack/components";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Index = () => {
-  const [data, setData] = useState(null); // Data from etherscan
-  const [isLoading, setLoading] = useState(false);
-  const [isUpdate, setIsUpdate] = useState(false); // Indicates wether to update the component or not with the new data
+  const { data, error } = useSWR(
+    `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN}`,
+    fetcher,
+    { refreshInterval: 5000 } // data should update every 5 seconds
+  );
 
-  const fetchGasPrice = () => {
-    /* it re-fetches the data from etherscan */
-    fetch(
-      `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  };
-
-  /* Only re-fetch the data on-demand */
-  useEffect(() => {
-    if (isUpdate && !isLoading) {
-      fetchGasPrice();
-    }
-  }, [isUpdate, isLoading]);
-
-  /* There's a 6 second wait interval before fetching data */
-  useEffect(() => {
-    var timerID = setInterval(async () => {
-      setIsUpdate(!isUpdate);
-    }, 6000);
-    return () => clearInterval(timerID);
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  console.log(data);
 
   return (
     <Layout>
       <DarkModeSwitch />
       <Center flexDir="column" mt="50%">
-        <button onClick={fetchGasPrice}>Refresh component</button>
+        {data.result.LastBlock}
       </Center>
     </Layout>
   );
