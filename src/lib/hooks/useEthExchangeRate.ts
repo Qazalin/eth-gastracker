e30b7fca8babf8ceb3149aa11b08d67df5b2cc56
qazal <ghazal.nzhrzr@gmail.com>
2022-05-04 11:12:07 +0430

    refactore

--- src/lib/hooks/useEthExchangeRate.ts
+++ src/lib/hooks/useEthExchangeRate.ts
@@ -5,7 +5,7 @@ import { basicFetcher } from "../basicFetcher";
 /**
  * Hook to fetch ETH exchange rate in USD and EUR
  */
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 export const useEthExchangeRate = () => {
   const { error, data } = useSWR<EthExchangeRateRes>(
     "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR",
