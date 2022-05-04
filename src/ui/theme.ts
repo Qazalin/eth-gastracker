a3f39a963d8e24afac66ef72f9cc8c86d690a996
qazal <ghazal.nzhrzr@gmail.com>
2022-05-04 11:25:16 +0430

    chore: theming

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -1,4 +1,4 @@
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 import { COLORS } from "./colors";
 import { mode } from "@chakra-ui/theme-tools";
 import { ThemeColors } from "./themeColors";
